66ce6ed7fb0f9dc9970c121a26b53365cd105513
Nazar Aziz <mcnazar@gmail.com>
2025-08-18 11:29:42 +0100

    stage 1 config savpoint

---
+++ lua/plugins/kanagawa.lua
@@ -0,0 +1,16 @@
+return {
+  "rebelot/kanagawa.nvim",
+  config = function()
+    require('kanagawa').setup({
+        compile = true,             -- enable compiling the colorscheme
+        theme = "wave",              -- Load "wave" theme
+    })
+
+    -- setup must be called before loading
+    vim.cmd("colorscheme kanagawa")
+  end,
+  build = function()
+    vim.cmd("KanagawaCompile")
+  end
+
+}
