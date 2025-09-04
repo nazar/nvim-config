fdd58ad8e6e046b39369383dc22dbc2aeef2cc6a
Nazar Aziz <mcnazar@gmail.com>
2025-09-04 17:43:09 +0100

    add back gitsigns key bindings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -154,6 +154,7 @@ return {
     -- See :help vim.diagnostic.Opts
     vim.diagnostic.config {
       severity_sort = true,
+      update_in_insert = false,
       float = { border = 'rounded', source = 'if_many' },
       underline = { severity = vim.diagnostic.severity.ERROR },
       signs = vim.g.have_nerd_font and {
